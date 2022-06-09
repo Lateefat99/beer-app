546b664a47260d68e072729dec13446366670c31
Lateefah <abubakarbisola@gmail.com>
2022-06-09 16:01:30 +0100

    Adding jackson config to BeerDto. closes #13

--- src/main/java/lateefat/sbms/beerapp/web/model/BeerDto.java
+++ src/main/java/lateefat/sbms/beerapp/web/model/BeerDto.java
@@ -1,14 +1,12 @@
 package lateefat.sbms.beerapp.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -25,9 +23,11 @@ public class BeerDto {
     @Null
     private Integer version;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime createdDate;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime modifiedDate;
 
@@ -35,15 +35,18 @@ public class BeerDto {
     private BeerStyle beerStyle;
 
     @NotBlank
+    @Size(min = 3, max = 100)
     private String beerName;
 
     @Positive
     @NotNull
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
+    @Positive
     private Integer quantityOnHand;
 
 }
--- src/test/java/lateefat/sbms/beerapp/web/controller/BeerControllerTest.java
+++ src/test/java/lateefat/sbms/beerapp/web/controller/BeerControllerTest.java
@@ -1,22 +1,45 @@
 package lateefat.sbms.beerapp.web.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lateefat.sbms.beerapp.domain.Beer;
+import lateefat.sbms.beerapp.repositories.BeerRepository;
 import lateefat.sbms.beerapp.web.model.BeerDto;
 import lateefat.sbms.beerapp.web.model.BeerStyle;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.util.StringUtils.*;
+
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "lateefat.sbms.beerapp.web.mappers")
 class BeerControllerTest {
 
     @Autowired
@@ -25,10 +48,34 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    BeerRepository beerRepository;
+
     @Test
     void getBeerById() throws Exception{
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID())
+                .param("iscold", "yes")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("v1/getBeer",
+                        pathParameters(
+                                parameterWithName("beerId").description("UUID of desired beer to get")
+                        ),
+                        requestParameters(
+                                parameterWithName("iscold").description("The beer is cold Query param")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("Id of beer").type(UUID.class),
+                                fieldWithPath("version").description("Beer Version"),
+                                fieldWithPath("createdDate").description("Beer Created Date").type(OffsetDateTime.class),
+                                fieldWithPath("modifiedDate").description("Beer Modified Date").type(OffsetDateTime.class),
+                                fieldWithPath("beerName").description("Beer Name"),
+                                fieldWithPath("beerStyle").description("Beer Style"),
+                                fieldWithPath("upc").description("UPC of Beer"),
+                                fieldWithPath("price").description("Beer Price"),
+                                fieldWithPath("quantityOnHand").description("Quantity On Hand")
+                        )));
     }
 
     @Test
@@ -36,10 +83,24 @@ class BeerControllerTest {
         BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
+        ConstrainedFields fields = new ConstrainedFields(BeerDto.class);
+
         mockMvc.perform(post("/api/v1/beer/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andDo(document("v1/newBeer",
+                        requestFields(
+                                fields.withPath("id").ignored(),
+                                fields.withPath("version").ignored(),
+                                fields.withPath("createdDate").ignored(),
+                                fields.withPath("modifiedDate").ignored(),
+                                fields.withPath("beerName").description("Beer Name"),
+                                fields.withPath("beerStyle").description("Beer Style"),
+                                fields.withPath("upc").description("UPC of Beer").attributes(),
+                                fields.withPath("price").description("Beer Price"),
+                                fields.withPath("quantityOnHand").ignored()
+                        )));
     }
 
     @Test
@@ -59,5 +120,19 @@ class BeerControllerTest {
                 .price(new BigDecimal("2.90"))
                 .upc(2223344556L)
                 .build();
+    }
+
+    private static class ConstrainedFields{
+        private final ConstraintDescriptions constraintDescriptions;
+
+        ConstrainedFields(Class<?> input) {
+            this.constraintDescriptions = new ConstraintDescriptions(input);
+        }
+
+        private FieldDescriptor withPath(String path){
+            return fieldWithPath(path).attributes(key("constraints").value(StringUtils
+                    .collectionToDelimitedString(this.constraintDescriptions
+                            .descriptionsForProperty(path), ", ")));
+        }
     }
 }
